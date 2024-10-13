d5ce8f0503a4b9af22bc3fc01a28d58f887a15e0
steven <wc@gaudiolab.com>
2024-10-14 03:48:47 +0900

    .

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,21 +12,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> numbers = [];
+  List<int> numbers = List<int>.generate(45, (index) => index + 1);
   List<List<int>> history = [];
 
   @override
-  void initState() {
-    super.initState();
-    numbers = List<int>.generate(45, (index) => index + 1);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appbar(),
-      body: body(),
-    );
+    return Scaffold(appBar: appbar(), body: body());
   }
 
   AppBar appbar() {
@@ -34,13 +25,14 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.black,
       title: Row(
         children: [
-          Text('Lottery Number Generator',
-              style: GoogleFonts.acme(color: Colors.white)),
-          const SizedBox(width: 12),
+          const SizedBox(width: 8),
+          Text('Lottery', style: GoogleFonts.acme(color: Colors.white)),
+          const SizedBox(width: 8),
           Image.network(
               'https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fchoiwooseok.github.io&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false'),
         ],
       ),
+      titleSpacing: 0,
       actions: [
         IconButton(
           tooltip: 'Shuffle History',
@@ -50,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
             setState(() {});
           },
         ),
-        const SizedBox(width: 12),
+        const SizedBox(width: 8),
         IconButton(
           tooltip: 'Clear History',
           icon:
@@ -60,10 +52,10 @@ class _MyHomePageState extends State<MyHomePage> {
             setState(() {});
           },
         ),
-        const SizedBox(width: 12),
+        const SizedBox(width: 8),
         IconButton(
           tooltip: 'Generate Numbers',
-          icon: const Icon(Icons.generating_tokens, color: Colors.white),
+          icon: const Icon(Icons.add_box_outlined, color: Colors.white),
           onPressed: () {
             numbers.shuffle(Random.secure());
             var selectedNumbers = numbers.sublist(0, 6);
@@ -72,23 +64,14 @@ class _MyHomePageState extends State<MyHomePage> {
             setState(() {});
           },
         ),
-        const SizedBox(width: 12),
+        const SizedBox(width: 8),
       ],
     );
   }
 
   Container body() {
     return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.black,
-            Colors.black.withOpacity(0.3),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
+      color: Colors.black,
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -97,7 +80,12 @@ class _MyHomePageState extends State<MyHomePage> {
               child: ListView.builder(
                 itemCount: history.length,
                 itemBuilder: (context, index) {
-                  return item(index, context);
+                  return Column(
+                    children: [
+                      item(index, context),
+                      const SizedBox(height: 8),
+                    ],
+                  );
                 },
               ),
             ),
@@ -109,18 +97,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Center item(int index, BuildContext context) {
     return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _balls(history[index]),
-            const SizedBox(width: 12),
-            _remove(index),
-            const SizedBox(width: 12),
-            _copyToClipBoard(index, context)
-          ],
-        ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _balls(index),
+          const SizedBox(width: 8),
+          _remove(index),
+        ],
       ),
     );
   }
@@ -134,6 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return IconButton(
       tooltip: 'Remove History',
       icon: const Icon(Icons.delete, color: Colors.white),
+      padding: const EdgeInsets.all(0),
       onPressed: () {
         history.removeAt(index);
         setState(() {});
@@ -141,55 +125,48 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  IconButton _copyToClipBoard(int index, BuildContext context) {
-    return IconButton(
-      tooltip: 'Copy to Clipboard',
-      icon: const Icon(Icons.copy_all, color: Colors.white),
-      onPressed: () {
-        Clipboard.setData(ClipboardData(
-          text: history[index].join(', '),
-        ));
+  GestureDetector _balls(int index) {
+    return GestureDetector(
+      onTap: () {
+        Clipboard.setData(ClipboardData(text: history[index].join(', ')));
         ScaffoldMessenger.of(context)
           ..clearSnackBars()
           ..showSnackBar(
             SnackBar(
               behavior: SnackBarBehavior.floating,
-              margin: EdgeInsets.only(
-                top: 20.0,
-                bottom: 20.0,
-                left: MediaQuery.of(context).size.width * 0.75,
-                right: MediaQuery.of(context).size.width * 0.05,
-              ),
               backgroundColor: Colors.green,
-              content: Text(
-                'Copied to Clipboard',
-                style: GoogleFonts.acme(color: Colors.white),
+              content: FittedBox(
+                fit: BoxFit.scaleDown,
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Copied to Clipboard',
+                  maxLines: 1,
+                  style: GoogleFonts.acme(color: Colors.white, fontSize: 12),
+                ),
               ),
               duration: const Duration(seconds: 1),
               showCloseIcon: true,
             ),
           );
       },
-    );
-  }
-
-  Container _balls(List<int> selected) {
-    return Container(
-      width: 300,
-      height: 60,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.blueAccent, width: 1),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (var i = 0; i < 6; i++) ...[
-              _ball(selected[i]),
-              const SizedBox(width: 8),
-            ],
-          ],
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                for (var i = 0; i < 6; i++) ...[
+                  _ball(history[index][i]),
+                  const SizedBox(width: 4),
+                ],
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -197,11 +174,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   CircleAvatar _ball(int value) {
     return CircleAvatar(
+      radius: 20,
       backgroundColor: _getColor(value),
       child: Text(
         '$value',
         style: GoogleFonts.acme(
           color: Colors.white,
+          fontSize: 12,
           fontWeight: FontWeight.bold,
         ),
       ),
